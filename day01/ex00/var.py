a570fd17fb1bc420d14329bc8aff1e9e03a8c485
Fausto Giusepe faggion <fagiusep@c2r7p4.42sp.org.br>
2023-01-31 16:22:33 -0300

    day01 ex01

---
+++ day01/ex00/var.py
@@ -0,0 +1,21 @@
+
+
+i = 42
+str = "Amor"
+f = 2.5
+b = True
+l = [42, 36]
+dic = {42 : 42}
+t = ("apple", "banana", "cherry")
+s = {"apple", "banana", "cherry"}
+
+
+print("42 has a type", type(i))
+print("42 has a type", type(i))
+print("quarante-deux has a type", type(str))
+print("42.0 has a type", type(f))
+print("True has a type", type(b))
+print("[42] has a type", type(l))
+print("{42: 42} has a type", type(dic))
+print("(42,) has a type", type(t))
+print("set() has a type", type(s))
