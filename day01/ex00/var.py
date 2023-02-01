107582d8011321e6d4f2d6d5ee95382db3a826b9
Fausto Giusepe faggion <fagiusep@c2r6p4.42sp.org.br>
2023-02-01 10:17:00 -0300

    day01 ex00 done

--- day01/ex00/var.py
+++ day01/ex00/var.py
@@ -1,21 +1,24 @@
 
+def myvar():
+    
+    i = 42
+    print(i, " has a type", type(i))
+    str1 = "42"
+    print(str1, " has a type", type(str1))
+    str2 = "quarante-deux"
+    print(str2, " has a type", type(str2))
+    f = 42.0
+    print(f, " has a type", type(f))
+    b = True
+    print(b, " has a type", type(b))
+    l = [42]
+    print(l, " has a type", type(l))
+    dic = {42 : 42}
+    print(dic, " has a type", type(dic))
+    t = (42,)
+    print(t, " has a type", type(t))
+    s = set()
+    print(s, " has a type", type(s))
 
-i = 42
-str = "Amor"
-f = 2.5
-b = True
-l = [42, 36]
-dic = {42 : 42}
-t = ("apple", "banana", "cherry")
-s = {"apple", "banana", "cherry"}
-
-
-print("42 has a type", type(i))
-print("42 has a type", type(i))
-print("quarante-deux has a type", type(str))
-print("42.0 has a type", type(f))
-print("True has a type", type(b))
-print("[42] has a type", type(l))
-print("{42: 42} has a type", type(dic))
-print("(42,) has a type", type(t))
-print("set() has a type", type(s))
+if __name__ == '__main__':
+    myvar()
