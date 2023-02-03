76d327dbf8306d7ad677cda7e78b5bdaddda181a
Fausto Giusepe faggion <fagiusep@c2r4p1.42sp.org.br>
2023-02-03 10:07:26 -0300

    day01 ex05 done

--- day01/ex05/all_in.py
+++ day01/ex05/all_in.py
@@ -1,18 +1,24 @@
 import sys
 
-def pdf_dictionaries(list):
+def states_list():
     states = {
         "Oregon" : "OR",
         "Alabama" : "AL",
         "New Jersey": "NJ",
         "Colorado" : "CO"
     }
+    return states
+
+def capitals_list():
     capital_cities = {
         "OR": "Salem",
         "AL": "Montgomery",
         "NJ": "Trenton",
         "CO": "Denver"
     }
+    return capital_cities
+
+def display(list, state, capital_cities):
     for i in range(len(list)):
         found = False
         for key, value in states.items():
@@ -50,4 +56,7 @@ def parse_arg():
 if  __name__ == '__main__':
     chk_arg()
     list = parse_arg()
-    pdf_dictionaries(list)
+    states = states_list()
+    capital_cities = capitals_list()
+    display(list, states, capital_cities)
+    
